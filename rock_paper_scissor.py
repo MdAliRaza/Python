caa2896569c99584a2249bee2c73737e06f2c2da
MdAliRaza <mdaliraza8d@gmail.com>
2022-01-28 15:21:42 +0530

    adding rock_paper_scissor

---
+++ rock_paper_scissor.py
@@ -0,0 +1,46 @@
+import random 
+choices = ['rock', 'paper', 'scissor']
+computer = random.choice(choices)
+player = None
+
+
+def game():
+    global player
+    while player not in choices:
+        player = input('choose yours.. ').lower()
+
+        if player == computer:
+            print('Player : ', player)
+            print('Computer : ', computer)
+            print('Tie')
+        elif player == 'rock':
+            if computer == 'paper':
+                print('Player : ', player)
+                print('Computer : ', computer)
+                print('Computer wins')
+            elif computer == 'scissors':
+                print('Player : ', player)
+                print('Computer : ', computer)
+                print('Player Wins')
+        elif player == 'scissors':
+            if computer == 'rock':
+                print('Player : ', player)
+                print('Computer : ', computer)
+                print('Computer Wins')
+            elif computer == 'paper':
+                print('Player : ', player)
+                print('Computer : ', computer)
+                print('Player Wins')
+        elif player == 'paper':
+            if computer == 'rock':
+                print('Player : ', player)
+                print('Computer : ', computer)
+                print('Player Wins')
+            elif computer == 'scissors':
+                print('Player : ', player)
+                print('Computer : ', computer)
+                print('Computer Wins')
+
+
+for i in range(3):
+    game()
