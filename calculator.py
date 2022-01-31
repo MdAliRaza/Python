5f0df98d05560e2d83c93e9f31c524ed50a6220d
MdAliRaza <mdaliraza8d@gmail.com>
2022-01-31 23:05:49 +0530

    adding calculator

---
+++ calculator.py
@@ -0,0 +1,22 @@
+print('''
+CALCULATOR
+Add = A
+Substract = S
+Multiply = M
+Divide = D
+''')
+
+no1 = int(input("Enter first no. >> "))
+no2 = int(input("Enter second no. >> "))
+process = input("Enter what you want to process >> ").upper()
+
+if process == "A":
+    print("Added : ", no1 + no2)
+elif process == "S":
+    print("Substract : ", no1 - no2)
+elif process == "M":
+    print("Multiply : ", no1 * no2)
+elif process == "D":
+    print("Divide : ", no1 / no2)
+else:
+    print("Wrong Input Try Again!")
